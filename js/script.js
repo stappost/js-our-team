2ec610be0cb45814adf6769a683d363fbcbfdab9
stappost <emanuele.scarpantoni@gmail.com>
2023-12-04 13:50:29 +0100

    add array dipendenti

---
+++ js/script.js
@@ -0,0 +1,34 @@
+// creazione array "dipendenti" con all'interno oggetti 
+let dipendenti = [
+    // per ogni oggetto aggiungere nome, ruolo e immagine; 
+    {
+        name: "Wayne Barnett",
+        role: "Founder & CEO",
+        image: "wayne-barnett-founder-ceo.jpg",
+    },
+    {
+        name: "Angela Caroll",
+        role: "Chief Editor",
+        image: "angela-caroll-chief-editor.jpg",
+    },
+    {
+        name: "Walter Gordon",
+        role: "Office Manager",
+        image: "walter-gordon-office-manager.jpg",
+    },
+    {
+        name: "Angela Lopez",
+        role: "Social Media Manager",
+        image: "angela-lopez-social-media-manager.jpg",
+    },
+    {
+        name: "Scott Estrada",
+        role: "Developer",
+        image: "scott-estrada-developer.jpg",
+    },
+    {
+        name: "Barbara Ramos",
+        role: "Grafic Designer",
+        image: "barbara-ramos-grafic-designer.jpg",
+    }
+]
